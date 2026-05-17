277462b0e09ffd3960c700472676e3b3d2005dc1
Sascha Affolter <sascha_affolter@hotmail.com>
2026-05-17 21:46:24 +0700

    Verify merged runtime config for onprem and hybrid

--- MailboxAutomation/tests/Pester/ConfigurationSeparation.Tests.ps1
+++ MailboxAutomation/tests/Pester/ConfigurationSeparation.Tests.ps1
@@ -16,6 +16,37 @@ BeforeAll {
     $script:app = $script:appRaw | ConvertFrom-Json
     $script:onPrem = $script:onPremRaw | ConvertFrom-Json
     $script:hybrid = $script:hybridRaw | ConvertFrom-Json
+
+    Import-Module -Name (Join-Path -Path $root -ChildPath 'core\JobEngine.psm1') -Force
+
+    $mergeConfig = {
+        param(
+            [Parameter(Mandatory = $true)][string]$AppSettingsPath,
+            [Parameter(Mandatory = $true)][string]$EnvironmentPath,
+            [Parameter(Mandatory = $true)][string]$RootPath
+        )
+
+        InModuleScope -ModuleName JobEngine -Parameters @{
+            AppSettingsPath = $AppSettingsPath
+            EnvironmentPath = $EnvironmentPath
+            RootPath = $RootPath
+        } -ScriptBlock {
+            param(
+                [Parameter(Mandatory = $true)][string]$AppSettingsPath,
+                [Parameter(Mandatory = $true)][string]$EnvironmentPath,
+                [Parameter(Mandatory = $true)][string]$RootPath
+            )
+
+            $baseConfig = Read-JsonAsHashtable -Path $AppSettingsPath
+            $environmentConfig = Read-JsonAsHashtable -Path $EnvironmentPath
+            $mergedConfig = Merge-Hashtable -Base $baseConfig -Override $environmentConfig
+            $mergedConfig['RootPath'] = $RootPath
+            $mergedConfig
+        }
+    }
+
+    $script:mergedOnPrem = & $mergeConfig -AppSettingsPath $script:appSettingsPath -EnvironmentPath $script:onPremPath -RootPath $root
+    $script:mergedHybrid = & $mergeConfig -AppSettingsPath $script:appSettingsPath -EnvironmentPath $script:hybridPath -RootPath $root
 }
 
 Describe 'Configuration separation and environment profiles' {
@@ -92,6 +123,49 @@ Describe 'Configuration separation and environment profiles' {
     It 'OnPrem RemotePowerShell URI stays sv01250 in both environment files' {
         $script:onPrem.ExchangeOnPrem.RemotePowerShell.ConnectionUri | Should -Be 'http://sv01250.ksbl.local/PowerShell'
         $script:hybrid.ExchangeOnPrem.RemotePowerShell.ConnectionUri | Should -Be 'http://sv01250.ksbl.local/PowerShell'
+    }
+
+    It 'merged OnPrem runtime config contains base defaults and productive OnPrem overrides' {
+        $script:mergedOnPrem.EnvironmentName | Should -Be 'OnPrem'
+        $script:mergedOnPrem.RootPath | Should -Be (Resolve-Path (Join-Path -Path $PSScriptRoot -ChildPath '..\..')).Path
+        $script:mergedOnPrem.Paths.QueueRoot | Should -Be 'queues'
+        $script:mergedOnPrem.CsvDelimiter | Should -Be '|'
+        $script:mergedOnPrem.WhatIfDefault | Should -Be $true
+
+        $script:mergedOnPrem.ExchangeOnline.Enabled | Should -Be $false
+        $script:mergedOnPrem.ExchangeOnline.AppId | Should -Be ''
+        $script:mergedOnPrem.ExchangeOnPrem.RemotePowerShell.User | Should -Be 'ksbl\ServiceIAMJobs10'
+        $script:mergedOnPrem.ExchangeOnPrem.RemotePowerShell.SecretPath | Should -Be 'D:\iam\Secrets\serviceiamjobs10.sec'
+        $script:mergedOnPrem.ExchangeOnPrem.RemotePowerShell.ConnectionUri | Should -Be 'http://sv01250.ksbl.local/PowerShell'
+        $script:mergedOnPrem.ExchangeOnPrem.RemotePowerShell.Authentication | Should -Be 'Kerberos'
+
+        $script:mergedOnPrem.Notifications.Cc | Should -Contain 'ksbl.vl.iam-administrators@ksbl.ch'
+        $script:mergedOnPrem.HomeDirectory.NamespaceRoot | Should -Be '\\ksbl.local\HomeDrives'
+        $script:mergedOnPrem.HomeDirectory.DefaultHomeDrive | Should -Be 'Z:'
+        $script:mergedOnPrem.EventLog.LogName | Should -Be 'KSBL Helpdesk GUI'
+    }
+
+    It 'merged Hybrid runtime config contains OnPrem runtime values plus Exchange Online values' {
+        $script:mergedHybrid.EnvironmentName | Should -Be 'Hybrid'
+        $script:mergedHybrid.Paths.QueueRoot | Should -Be 'queues'
+        $script:mergedHybrid.CsvDelimiter | Should -Be '|'
+        $script:mergedHybrid.WhatIfDefault | Should -Be $true
+
+        $script:mergedHybrid.ExchangeOnline.Enabled | Should -Be $true
+        $script:mergedHybrid.ExchangeOnline.AppId | Should -Be '98972cc8-f5bc-4dcd-8bcc-ac82925d2bf2'
+        $script:mergedHybrid.ExchangeOnline.Organization | Should -Be 'kantonsspitalbl.onmicrosoft.com'
+        $script:mergedHybrid.ExchangeOnline.TenantDomain | Should -Be 'kantonsspitalbl.onmicrosoft.com'
+        $script:mergedHybrid.ExchangeOnline.CertificateThumbprint | Should -Be 'EE3A37116AF905168D5334A8CA74970B692F4491'
+
+        $script:mergedHybrid.ExchangeOnPrem.RemotePowerShell.User | Should -Be 'ksbl\ServiceIAMJobs10'
+        $script:mergedHybrid.ExchangeOnPrem.RemotePowerShell.SecretPath | Should -Be 'D:\iam\Secrets\serviceiamjobs10.sec'
+        $script:mergedHybrid.ExchangeOnPrem.RemotePowerShell.ConnectionUri | Should -Be 'http://sv01250.ksbl.local/PowerShell'
+        $script:mergedHybrid.ExchangeOnPrem.RemotePowerShell.Authentication | Should -Be 'Kerberos'
+
+        $script:mergedHybrid.Notifications.Cc | Should -Contain 'ksbl.vl.iam-administrators@ksbl.ch'
+        $script:mergedHybrid.ActiveDirectory.DefaultDomain | Should -Be 'ksbl.local'
+        $script:mergedHybrid.HomeDirectory.ApplicationDirectoryShare | Should -Be '\\sv00213\Appdata$'
+        $script:mergedHybrid.Hospis.Database | Should -Be 'KSBL_IAM'
     }
 
     It 'all configuration JSON files are parseable' {
